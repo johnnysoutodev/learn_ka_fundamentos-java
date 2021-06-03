0818ea1a10f6085e0e0e05c44509c1455307efd9
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-03 19:01:17 -0300

    Create ClienteTO.java

---
+++ Alura/src/ClienteTO.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author johnny
+ */
+public class ClienteTO {
+    
+    private int odigo;
+    private String nome;
+
+    public int getOdigo() {
+        return odigo;
+    }
+
+    public void setOdigo(int odigo) {
+        this.odigo = odigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+    
+}
