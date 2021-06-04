fa37cf2043331fdb7828528c330b00bb5a7edc0e
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-03 21:00:52 -0300

    Create TrabalhandoReferenciaObjetoClienteTO.java

---
+++ Alura/src/TrabalhandoReferenciaObjetoClienteTO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author johnny
+ */
+public class TrabalhandoReferenciaObjetoClienteTO {
+    
+    public static void main (String args[]){
+    
+        ClienteTO obj1 = new ClienteTO();
+        ClienteTO obj2 = new ClienteTO();
+        
+        
+        
+    }
+    
+}
