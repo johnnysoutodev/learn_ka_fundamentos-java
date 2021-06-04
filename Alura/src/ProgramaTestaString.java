15428888a54cdedff05ffd655bebe5a19cc0a8e8
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-03 22:33:16 -0300

    Create ProgramaTestaString.java

---
+++ Alura/src/ProgramaTestaString.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author johnny
+ */
+public class ProgramaTestaString {
+    
+    public static void main (String args[]){
+        
+    String str1 = "Johnny";
+    String str2 = "Joao";
+    
+    }
+    
+}
