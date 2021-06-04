f25859d0a14fe29a7d4ce40527dfebddf7c0823f
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-03 22:45:40 -0300

    Update ManipulacaoString.java

--- Alura/src/ManipulacaoString.java
+++ Alura/src/ManipulacaoString.java
@@ -14,11 +14,21 @@ public class ManipulacaoString {
         
 //        String str1 = new String("Joao");
         int age = 39;
-        String str1 = "Joao";
+        String str1 = "Johnny";
         String str2 = "Joao";
+        String str3 = "";
+        String nome = "Johnny";
+        String espaco = " ";
+        String sobrenome = "Souto";
         
         System.out.println(str1 == str2);
         System.out.println(str1.equals(age));
+        
+        System.out.println(str1.valueOf(str1));
+        
+        System.out.println(str3.isEmpty());
+        
+        System.out.println(nome.concat(espaco).concat(sobrenome));
             
     }
     
