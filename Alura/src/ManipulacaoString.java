f0e0f4d90327e67b26754fdd9961e771ef7504ae
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-03 22:33:14 -0300

    Create ManipulacaoString.java

---
+++ Alura/src/ManipulacaoString.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author johnny
+ */
+public class ManipulacaoString {
+    
+    public static void main (String args[]){
+        
+//        String str1 = new String("Joao");
+        int age = 39;
+        String str1 = "Joao";
+        String str2 = "Joao";
+        
+        System.out.println(str1 == str2);
+        System.out.println(str1.equals(age));
+            
+    }
+    
+    
+    
+}
