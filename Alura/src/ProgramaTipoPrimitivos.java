cb8a522860b1e34f1874805e19aa3cfb987be484
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-05 03:06:10 -0300

    Create ProgramaTipoPrimitivos.java

---
+++ Alura/src/ProgramaTipoPrimitivos.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author johnny
+ */
+public class ProgramaTipoPrimitivos {
+    
+    public static void main (String args[]){
+        
+        // Variaveis do Tipo Primitivo
+        byte b = 100;
+        short s = 100;
+        int i = 10;
+        long l = 1000000000L;
+        
+        float f = 10.7f;
+        double d = 400000000.0000099;
+        
+        char ch = 'J';
+        
+        boolean boo = false;
+        
+        
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        
+        System.out.println(f);
+        System.out.println(d);
+        
+        System.out.println(ch);
+        
+        System.out.println(boo);
+        
+        // Tipo construido = Wrapper Class
+        Byte objByte = 127;
+        Short objShort = 32764;
+        Integer objInteger = 27000000;
+        Long objLong = 27000000000000L;
+        Float objFloat = 27000000000.00027000027F;
+        Double objDouble = 400000000.0099999;
+        
+        Character objChar = 'A';
+        Boolean objBoolean = true;
+        
+    
+    }
+    
+}
