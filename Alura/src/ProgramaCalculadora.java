84cad81e3e529f8200e2872900bf173bbc2fcdc8
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-07 21:42:32 -0300

    Create ProgramaCalculadora.java

---
+++ Alura/src/ProgramaCalculadora.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author johnny
+ */
+public class ProgramaCalculadora {
+    
+    public static void main(String args[]){
+        System.out.println(Calculadora.soma(3, 7));
+        System.out.println(Calculadora.subtrair(3, 7));
+        System.out.println(Calculadora.dividir(10, 3));
+        System.out.println(Calculadora.multiplicar(4, 2));
+    }
+    
+}
