d85a95aac67258706386f6790d4876cb8c0858da
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-07 21:42:26 -0300

    Create Calculadora.java

---
+++ Alura/src/Calculadora.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author johnny
+ */
+public class Calculadora {
+    
+    public static int soma(int num1, int num2){
+        return (num1 + num2);
+    }
+    
+    public static double soma(double num1, double num2){
+        return (num1 + num2);
+    }
+    
+    public static int subtrair(int num1, int num2){
+        return (num1 - num2);
+    }
+    
+    public static double subtrair(double num1, double num2){
+        return (num1 - num2);
+    }
+    
+    public static int dividir(int num1, int num2){
+        return (num1 / num2);
+    }
+    
+    public static double dividir(double num1, double num2){
+        return (num1 / num2);
+    }
+    
+    public static int multiplicar(int num1, int num2){
+        return (num1 * num2);
+    }
+    
+    public static double multiplicar(double num1, double num2){
+        return (num1 * num2);
+    }
+    
+}
