0a53b5362a5df0c78396a423676134c8d94f5b20
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-08 21:18:48 -0300

    Create ProgramaTestaConstrutorEnderecoTO.java

---
+++ Alura/src/ProgramaTestaConstrutorEnderecoTO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author johnny
+ */
+public class ProgramaTestaConstrutorEnderecoTO {
+    
+    public static void main (String args[]){
+        EnderecoTO objEnderecoTO = new EnderecoTO();
+        EnderecoTO objEnderecoTO2 = new EnderecoTO();
+        objEnderecoTO.listEndereco();
+        System.out.println(objEnderecoTO == objEnderecoTO2);
+        System.out.println(objEnderecoTO.equals(objEnderecoTO2));
+        System.out.println();
+    }
+    
+}
