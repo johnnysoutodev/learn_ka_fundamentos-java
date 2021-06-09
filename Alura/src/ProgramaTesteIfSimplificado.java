e836f3c96dd20411ff1b1ca4930e5d2231eef751
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-08 21:18:45 -0300

    Create ProgramaTesteIfSimplificado.java

---
+++ Alura/src/ProgramaTesteIfSimplificado.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author johnny
+ */
+public class ProgramaTesteIfSimplificado {
+    public static void main (String args[]){
+        int valor = 90;
+        String resultado = ( valor < 100 ? "Valor menor que 100" : "Valor maior que 100");
+        System.out.println(resultado);
+    }
+    
+}
