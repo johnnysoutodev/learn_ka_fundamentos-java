f9298a9b30d38dcdae5c9798f2bc1f64dff4e693
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-08 21:36:03 -0300

    Update FornecedorTO.java

--- Alura/src/FornecedorTO.java
+++ Alura/src/FornecedorTO.java
@@ -1,3 +1,6 @@
+
+import java.util.Objects;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -17,6 +20,11 @@ public class FornecedorTO {
     private String razaoSocial;
 
     public FornecedorTO() {
+        this.setCodigo(1);
+        this.setNome("Empresa XPTO");
+        this.setEmail("contato@xpto.com");
+        this.setCNPJ("54123999000199");
+        this.setRazaoSocial("XPTO S/A");
     }
 
     public FornecedorTO(int codigo, String nome, String email, String CNPJ, String razaoSocial) {
@@ -26,8 +34,6 @@ public class FornecedorTO {
         this.CNPJ = CNPJ;
         this.razaoSocial = razaoSocial;
     }
-    
-    
 
     public int getCodigo() {
         return codigo;
@@ -69,6 +75,54 @@ public class FornecedorTO {
         this.razaoSocial = razaoSocial;
     }
     
+    public void printFornecedor(){
+        System.out.println("Código:         " + this.getCodigo());
+        System.out.println("Nome:           " + this.getNome());
+        System.out.println("E-mail:         " + this.getEmail());
+        System.out.println("CNPJ:           " + this.getCNPJ());
+        System.out.println("Razão Social:   " + this.getRazaoSocial());
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + this.codigo;
+        hash = 79 * hash + Objects.hashCode(this.nome);
+        hash = 79 * hash + Objects.hashCode(this.email);
+        hash = 79 * hash + Objects.hashCode(this.CNPJ);
+        hash = 79 * hash + Objects.hashCode(this.razaoSocial);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final FornecedorTO other = (FornecedorTO) obj;
+        if (this.codigo != other.codigo) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        if (!Objects.equals(this.CNPJ, other.CNPJ)) {
+            return false;
+        }
+        if (!Objects.equals(this.razaoSocial, other.razaoSocial)) {
+            return false;
+        }
+        return true;
+    }
     
     
     
