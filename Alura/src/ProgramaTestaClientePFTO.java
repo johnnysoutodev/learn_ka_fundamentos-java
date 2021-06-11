20313c09e9ce220d87cc2b3bfda0c0f6fdaa7e34
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-10 21:08:23 -0300

    Update ProgramaTestaClientePFTO.java

--- Alura/src/ProgramaTestaClientePFTO.java
+++ Alura/src/ProgramaTestaClientePFTO.java
@@ -13,10 +13,19 @@ public class ProgramaTestaClientePFTO {
     public static void main(String args[]){
         
         ClientePFTO obj = new ClientePFTO(1,"Johnny","99999999999");
+        ClientePFTO obj2 = new ClientePFTO(1,"Johnny","99999999999");
         System.out.println("ID" + "| " + "Nome   " + " | " + "CPF ");
         System.out.println(obj.getCodigo() + " | " + obj.getNome() + " | " + obj.getCPF());
         System.out.println("-------------------------- ");
 //        System.out.println(obj.toString());
+        ClientePFTO objFilho = (ClientePFTO) obj2;
+        System.out.println(obj2 instanceof ClienteTO);
+        System.out.println(obj2 instanceof ClientePFTO);
+        System.out.println(objFilho.toString());
+        /* ArrayList<ClienteTO> array = new ArrayList();
+        array.add(obj);
+        array.add(obj2); */
+        
     }
     
 }
