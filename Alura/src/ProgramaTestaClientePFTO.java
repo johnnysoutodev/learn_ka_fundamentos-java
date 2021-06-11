c0da6c5c3c7cd4d3c9cdb4ec067b579566dd9f9a
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-10 21:23:19 -0300

    Update ProgramaTestaClientePFTO.java

--- Alura/src/ProgramaTestaClientePFTO.java
+++ Alura/src/ProgramaTestaClientePFTO.java
@@ -1,3 +1,6 @@
+
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,12 +11,15 @@
  *
  * @author johnny
  */
+import java.util.ArrayList;
+
 public class ProgramaTestaClientePFTO {
     
     public static void main(String args[]){
         
         ClientePFTO obj = new ClientePFTO(1,"Johnny","99999999999");
         ClientePFTO obj2 = new ClientePFTO(1,"Johnny","99999999999");
+        ClienteTO obj3 = new ClienteTO();
         System.out.println("ID" + "| " + "Nome   " + " | " + "CPF ");
         System.out.println(obj.getCodigo() + " | " + obj.getNome() + " | " + obj.getCPF());
         System.out.println("-------------------------- ");
@@ -22,9 +28,10 @@ public class ProgramaTestaClientePFTO {
         System.out.println(obj2 instanceof ClienteTO);
         System.out.println(obj2 instanceof ClientePFTO);
         System.out.println(objFilho.toString());
-        /* ArrayList<ClienteTO> array = new ArrayList();
+        ArrayList<ClienteTO> array = new ArrayList();
         array.add(obj);
-        array.add(obj2); */
+        array.add(obj2);
+        array.add(obj3);
         
     }
     
