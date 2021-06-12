f140fe25cf21c43db9450c5446f48056522e5147
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-11 22:08:28 -0300

    Update ProgramaAula3.java

--- Alura/src/ProgramaAula3.java
+++ Alura/src/ProgramaAula3.java
@@ -1,3 +1,7 @@
+
+import kasolution.excecoes.DadosInvalidosException;
+import kasolution.TO.ClienteTO;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,7 +14,7 @@
  */
 public class ProgramaAula3 {
     
-    public static void main (String args[]) {
+    public static void main (String args[]) throws DadosInvalidosException{
     
         System.out.println("Aula 3 do dia 02/06/2021");
         
