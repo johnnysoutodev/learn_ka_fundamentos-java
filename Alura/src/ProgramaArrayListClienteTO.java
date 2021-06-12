4d3f240418f7550be6fdd192bf6e915936844e38
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-11 22:08:30 -0300

    Update ProgramaArrayListClienteTO.java

--- Alura/src/ProgramaArrayListClienteTO.java
+++ Alura/src/ProgramaArrayListClienteTO.java
@@ -8,16 +8,31 @@
  *
  * @author johnny
  */
+import kasolution.excecoes.DadosInvalidosException;
+import kasolution.TO.ClienteTO;
 import java.util.ArrayList;
 
 public class ProgramaArrayListClienteTO {
-    public static void main (String args[]){
+    public static void main (String args[]) throws DadosInvalidosException{
+        ClienteTO[] arrCli = listaClientes(100);
+        for(ClienteTO objCli : arrCli){
+            objCli.toString();
+            System.out.println(objCli.getNome());
+        }
+        
+    }
+    
+    public static ClienteTO[] listaClientes(int qtd) throws DadosInvalidosException{
         
         ArrayList<ClienteTO> arr = new ArrayList<ClienteTO>();
-        for(int x = 0; x < 100; x++){
+        for(int x = 0; x < qtd; x++){
             arr.add(new ClienteTO(x+1,"Cliente " + (x+1)));
         }
-        System.out.println(arr.get(5).getNome());
+        
+        ClienteTO[] arrClienteTO = new ClienteTO[qtd];
+        return arr.toArray(arrClienteTO);
     }
+    
+    
     
 }
