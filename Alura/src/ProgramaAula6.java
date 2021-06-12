58ac7abc7960abb733be24f95db60331a0f46ae2
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-11 22:08:24 -0300

    Update ProgramaAula6.java

--- Alura/src/ProgramaAula6.java
+++ Alura/src/ProgramaAula6.java
@@ -1,3 +1,7 @@
+
+import kasolution.excecoes.DadosInvalidosException;
+import kasolution.TO.ClienteTO;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,7 +14,7 @@
  */
 public class ProgramaAula6 {
     
-    public static void main(String args[]){
+    public static void main(String args[]) throws DadosInvalidosException{
         ClienteTO objClienteTO = ClienteTO.criaClienteTO(10, "Ka-Solution");
         System.out.println(objClienteTO.getCodigo() + " " + objClienteTO.getNome());
         
